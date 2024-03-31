9d88254fff98523168cac7ff1678f4acd459eef9
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2024-03-31 15:49:07 +0500

    Included Account Type

--- index.js
+++ index.js
@@ -9,24 +9,35 @@ let userPin = await inquirer.prompt({
 });
 if (userPin.pin === myPin) {
     console.log("Welcome To ATM");
-    let userOperation = await inquirer.prompt({
-        name: "operation",
-        message: "Please select any operation: ",
-        type: "list",
-        choices: ["Check Balance", "WidthDraw"],
-    });
+    let userOperation = await inquirer.prompt([
+        {
+            name: "accountType",
+            type: "list",
+            message: "Please select account type: ",
+            choices: ["Current Account", "Saving Account"],
+        },
+        {
+            name: "operation",
+            message: "Please select any operation: ",
+            type: "list",
+            choices: ["Check Balance", "Cash WithDrawal"],
+        },
+    ]);
+    //Check Balance
     if (userOperation.operation === "Check Balance") {
         console.log(`Your current balance: Rs.${myBalance}`);
         console.log("Thank you for using ATM!");
     }
-    else if (userOperation.operation === "WidthDraw") {
-        let widthDrawMethod = await inquirer.prompt({
+    // Cash WithDrawal
+    else if (userOperation.operation === "Cash WithDrawal") {
+        let withDrawMethod = await inquirer.prompt({
             name: "withDrawMethod",
             type: "list",
             message: "Please select any option: ",
             choices: ["Enter Amount", "Fast Cash"],
         });
-        if (widthDrawMethod.withDrawMethod === "Fast Cash") {
+        // Fast Cash
+        if (withDrawMethod.withDrawMethod === "Fast Cash") {
             let fastCashAmount = await inquirer.prompt([
                 {
                     name: "amount",
@@ -35,34 +46,36 @@ if (userPin.pin === myPin) {
                     choices: [1000, 2000, 4000, 5000, 10000, 20000],
                 },
             ]);
-            if (fastCashAmount.amount > myBalance) {
-                console.log("Sorry you have insufficient balance!");
-            }
-            else {
+            if (fastCashAmount.amount <= myBalance) {
                 myBalance -= fastCashAmount.amount;
                 console.log(`You have successully withdraw Rs.${fastCashAmount.amount}`);
                 console.log(`Remaining balance: Rs.${myBalance}`);
                 console.log("Thank you for using ATM!");
             }
+            else {
+                console.log("Sorry you have insufficient balance!");
+            }
         }
-        else if (widthDrawMethod.withDrawMethod === "Enter Amount") {
-            let widthDrawAmount = await inquirer.prompt({
+        // Enter Cash Withdrawal Amount
+        else if (withDrawMethod.withDrawMethod === "Enter Amount") {
+            let withDrawAmount = await inquirer.prompt({
                 name: "amount",
                 type: "number",
                 message: "Please select any amount: ",
             });
-            if (widthDrawAmount.amount > myBalance) {
-                console.log("Sorry you have insufficient balance!");
-            }
-            else {
-                myBalance -= widthDrawAmount.amount;
-                console.log(`You have successully withdraw Rs.${widthDrawAmount.amount}`);
+            if (withDrawAmount.amount <= myBalance) {
+                myBalance -= withDrawAmount.amount;
+                console.log(`You have successully withdraw Rs.${withDrawAmount.amount}`);
                 console.log(`Remaining balance: Rs.${myBalance}`);
                 console.log("Thank you for using ATM!");
+            }
+            else {
+                console.log("Sorry you have insufficient balance!");
             }
         }
     }
 }
+// Wrong Pin
 else {
     console.log("Please Enter Correct Pin Number!");
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 
-let myBalance = 10000;
-let myPin = 1234;
+let myBalance: number = 10000;
+let myPin: number = 1234;
 
 let userPin = await inquirer.prompt({
   name: "pin",
@@ -13,25 +13,38 @@ let userPin = await inquirer.prompt({
 if (userPin.pin === myPin) {
   console.log("Welcome To ATM");
 
-  let userOperation = await inquirer.prompt({
-    name: "operation",
-    message: "Please select any operation: ",
-    type: "list",
-    choices: ["Check Balance", "WidthDraw"],
-  });
+  let userOperation = await inquirer.prompt([
+    {
+      name: "accountType",
+      type: "list",
+      message: "Please select account type: ",
+      choices: ["Current Account", "Saving Account"],
+    },
+    {
+      name: "operation",
+      message: "Please select any operation: ",
+      type: "list",
+      choices: ["Check Balance", "Cash WithDrawal"],
+    },
+  ]);
 
+  //Check Balance
   if (userOperation.operation === "Check Balance") {
     console.log(`Your current balance: Rs.${myBalance}`);
     console.log("Thank you for using ATM!");
-  } else if (userOperation.operation === "WidthDraw") {
-    let widthDrawMethod = await inquirer.prompt({
+  }
+
+  // Cash WithDrawal
+  else if (userOperation.operation === "Cash WithDrawal") {
+    let withDrawMethod = await inquirer.prompt({
       name: "withDrawMethod",
       type: "list",
       message: "Please select any option: ",
       choices: ["Enter Amount", "Fast Cash"],
     });
 
-    if (widthDrawMethod.withDrawMethod === "Fast Cash") {
+    // Fast Cash
+    if (withDrawMethod.withDrawMethod === "Fast Cash") {
       let fastCashAmount = await inquirer.prompt([
         {
           name: "amount",
@@ -41,9 +54,7 @@ if (userPin.pin === myPin) {
         },
       ]);
 
-      if (fastCashAmount.amount > myBalance) {
-        console.log("Sorry you have insufficient balance!");
-      } else {
+      if (fastCashAmount.amount <= myBalance) {
         myBalance -= fastCashAmount.amount;
 
         console.log(
@@ -51,26 +62,34 @@ if (userPin.pin === myPin) {
         );
         console.log(`Remaining balance: Rs.${myBalance}`);
         console.log("Thank you for using ATM!");
+      } else {
+        console.log("Sorry you have insufficient balance!");
       }
-    } else if (widthDrawMethod.withDrawMethod === "Enter Amount") {
-      let widthDrawAmount = await inquirer.prompt({
+    }
+
+    // Enter Cash Withdrawal Amount
+    else if (withDrawMethod.withDrawMethod === "Enter Amount") {
+      let withDrawAmount = await inquirer.prompt({
         name: "amount",
         type: "number",
         message: "Please select any amount: ",
       });
 
-      if (widthDrawAmount.amount > myBalance) {
-        console.log("Sorry you have insufficient balance!");
-      } else {
-        myBalance -= widthDrawAmount.amount;
+      if (withDrawAmount.amount <= myBalance) {
+        myBalance -= withDrawAmount.amount;
+
         console.log(
-          `You have successully withdraw Rs.${widthDrawAmount.amount}`
+          `You have successully withdraw Rs.${withDrawAmount.amount}`
         );
         console.log(`Remaining balance: Rs.${myBalance}`);
         console.log("Thank you for using ATM!");
+      } else {
+        console.log("Sorry you have insufficient balance!");
       }
     }
   }
-} else {
+}
+// Wrong Pin
+else {
   console.log("Please Enter Correct Pin Number!");
 }
